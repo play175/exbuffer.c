b583d5dfc675640ef8e65d9bec872bb3032ed3e8
yoyo <mvpsoft@gmail.com>
2013-02-18 22:59:06 +0800

    Update exbuffer.c

--- exbuffer.c
+++ exbuffer.c
@@ -3,10 +3,10 @@
  * yoyo 2013 https://github.com/play175/exbuffer.c
  * new BSD Licensed
  */
- #include <cstdio>
-#include <cstdlib>
-#include <cstring>
-#include <cmath>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
 
 #ifndef EXBUFFER_H
 #define EXBUFFER_H
@@ -31,7 +31,7 @@ enum exbuffer_endian
 
 void check_host_endian()
 {
-	if(HOST_ENDIAN_LITTLE & 0x01 == 0x01)
+	if((HOST_ENDIAN_LITTLE & 0x01) == 0x01)
 	{
 		return;
 	}
@@ -52,22 +52,22 @@ void check_host_endian()
 	p = NULL;
 }
 
-unsigned long ntohl(unsigned long x,exbuffer_endian endian)
+unsigned long ntohl(unsigned long x,enum exbuffer_endian endian)
 {
 	check_host_endian();
 
 	if(
-		(endian == EXBUFFER_BIG_ENDIAN  
-		&& (HOST_ENDIAN_LITTLE & 0x0100 == 0x0000)//big endian
+		(endian == EXBUFFER_BIG_ENDIAN
+		&& ((HOST_ENDIAN_LITTLE & 0x0100) == 0x0000)//big endian
 		)
-		||(endian == EXBUFFER_LITTLE_ENDIAN  
-		&& (HOST_ENDIAN_LITTLE & 0x0100 == 0x0100)//little endian
+		||(endian == EXBUFFER_LITTLE_ENDIAN
+		&& ((HOST_ENDIAN_LITTLE & 0x0100) == 0x0100)//little endian
 		)
 	)
 	{
 		return x;
 	}
-	
+
 	return
 	((unsigned long)(	\
         (((unsigned long)(x) & (unsigned long)0x000000ffUL) << 24) |	 \
@@ -76,22 +76,22 @@ unsigned long ntohl(unsigned long x,exbuffer_endian endian)
         (((unsigned long)(x) & (unsigned long)0xff000000UL) >> 24)));
 }
 
-unsigned short ntohs(unsigned short x,exbuffer_endian endian)
+unsigned short ntohs(unsigned short x,enum exbuffer_endian endian)
 {
 	check_host_endian();
 
 	if(
 		(endian == EXBUFFER_BIG_ENDIAN  \
-		&& (HOST_ENDIAN_LITTLE & 0x0100 == 0x0000)//big endian
+		&& ((HOST_ENDIAN_LITTLE & 0x0100) == 0x0000)//big endian
 		)
 		||(endian == EXBUFFER_LITTLE_ENDIAN  \
-		&& (HOST_ENDIAN_LITTLE & 0x0100 == 0x0100)//little endian
+		&&((HOST_ENDIAN_LITTLE & 0x0100) == 0x0100)//little endian
 		)
 	)
 	{
 		return x;
 	}
-	
+
 	return
 	((unsigned short)(	\
         (((unsigned short)(x) & (unsigned short)0x00ffU) << 8) |	 \
@@ -103,7 +103,7 @@ unsigned short ntohs(unsigned short x,exbuffer_endian endian)
 typedef struct exbuffer_value
 {
 	unsigned char headLen;/*!< 包头长度：2/4 */
-    exbuffer_endian endian;/*!<  */
+    enum exbuffer_endian endian;/*!<  */
     size_t readOffset;/*!<  */
     size_t putOffset;/*!<  */
     size_t dlen;/*!< 本次数据包长度 */
@@ -113,21 +113,24 @@ typedef struct exbuffer_value
 } exbuffer_t;
 
 /*!< 创建一个新的exbuffer_t数据结构 */
-exbuffer_t* exbuffer_new(unsigned char headLen = 2,exbuffer_endian endian = EXBUFFER_BIG_ENDIAN,size_t bufferlen = 512)
+exbuffer_t* exbuffer_new()
 {
+    unsigned char headLen = 2;
+    enum exbuffer_endian endian =  EXBUFFER_BIG_ENDIAN;
+    size_t bufferlen = 512;
 	exbuffer_t* value;
 	value =  (exbuffer_t *)malloc (sizeof (exbuffer_t));
 	value->bufferlen = bufferlen;
-	value->headLen = 2;
+	value->headLen = headLen;
 	value->endian = endian;
 	value->readOffset = 0;
 	value->putOffset = 0;
 	value->dlen = 0;
 	value->recvHandle = NULL;
-	
+
 	value->buffer = (unsigned char*)malloc(value->bufferlen);
 	//memset(value->buffer,0,value->bufferlen);
-	
+
 	return value;
 };
 
@@ -136,14 +139,15 @@ void exbuffer_free(exbuffer_t** value)
 {
 	free ((*value)->buffer);
 	(*value)->buffer = NULL;
-	(*value)->recvHandle = NULL;	
+	(*value)->recvHandle = NULL;
 	free (*value);
 	(*value) = NULL;
 };
 
-/*!<  */
-void exbuffer_printHex(unsigned char* bytes,unsigned short len = 50)
+/*!< 打印内存数据 */
+void exbuffer_printHex(unsigned char* bytes)
 {
+    unsigned short len = 50;
 	unsigned short iLoop;
 	for(iLoop = 0;iLoop < len;iLoop++)
     {
@@ -155,9 +159,7 @@ void exbuffer_printHex(unsigned char* bytes,unsigned short len = 50)
 /*!< 打印exbuffer_t中的缓冲区内存(最多打印50个字节) */
 void exbuffer_dump(exbuffer_t* value)
 {
-	unsigned short len = 50;
-	if(value->bufferlen<len)len = value->bufferlen;
-	exbuffer_printHex(value->buffer,len);
+	exbuffer_printHex(value->buffer);
 };
 
 /*!< 获取缓冲区有效数据长度 */
@@ -257,7 +259,7 @@ void exbuffer_proc(exbuffer_t* value)
 			if(value->readOffset + value->dlen > value->bufferlen)//***---*****
 			{
 				size_t len1 = value->bufferlen - value->readOffset;
-				if (len1 > 0) 
+				if (len1 > 0)
 				{
 					memcpy(dbuff,value->buffer + value->readOffset,len1);
 				}
@@ -274,7 +276,7 @@ void exbuffer_proc(exbuffer_t* value)
 			}
 			size_t dlen = value->dlen;
 			value->dlen = 0;
-			
+
 			if(value->recvHandle == NULL)
 			{
 				printf("receive packet:%d\n",dlen);
@@ -286,12 +288,12 @@ void exbuffer_proc(exbuffer_t* value)
 			free(dbuff);
 			dbuff = NULL;
 
-			if (value->readOffset == value->putOffset) 
+			if (value->readOffset == value->putOffset)
 			{
 				break;
-			}			
+			}
 		}
-		else 
+		else
 		{
 			break;
 		}
@@ -318,14 +320,14 @@ void exbuffer_put(exbuffer_t* value, unsigned char* buffer,size_t offset,size_t 
 		//重新指向新内存
 		value->buffer = tmp;
 		value->bufferlen = ex;
-		
+
 		//整理内存
-		if (value->putOffset < value->readOffset) //***** ---********------- 
+		if (value->putOffset < value->readOffset) //***** ---********-------
 		{
 			size_t cpylen;
 			size_t cpystctstart;
 			size_t cpydeststart;
-			if (value->putOffset <= exlen) //***** ---********------- 
+			if (value->putOffset <= exlen) //***** ---********-------
 			{
 				cpystctstart = 0;
 				cpydeststart = ex - exlen;
@@ -340,7 +342,7 @@ void exbuffer_put(exbuffer_t* value, unsigned char* buffer,size_t offset,size_t 
 			memcpy(value->buffer + cpystctstart,value->buffer + cpydeststart,cpylen);
 		}
 	}
-	
+
 	if(exbuffer_getLen(value) == 0){
 		value->putOffset = value->readOffset = 0;
 	}
@@ -349,7 +351,7 @@ void exbuffer_put(exbuffer_t* value, unsigned char* buffer,size_t offset,size_t 
 		//分两次存 一部分存在数据后面 一部分存在数据前面
 		size_t len1 = value->bufferlen - value->putOffset;
 		memcpy(value->buffer + value->putOffset,buffer + offset,len1);
-		offset += len1;		
+		offset += len1;
 		size_t len2 = len - len1;
 		memcpy(value->buffer,buffer + offset,len2);
 		value->putOffset = len2;
